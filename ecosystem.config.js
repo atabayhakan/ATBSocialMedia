c84310fa13d1d3e77b26f9b9fe1c9a78707569ad
Atabay Hakan <admin@powervital.kg>
2026-07-24 11:31:46 +0600

    Move atb-frontend to port 3005 to free up port 3000

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -17,7 +17,7 @@ module.exports = {
       name: 'atb-frontend',
       cwd: './frontend',
       script: 'node_modules/next/dist/bin/next',
-      args: 'start',
+      args: 'start -p 3005',
       env: { NODE_ENV: 'production' },
       autorestart: true,
       max_restarts: 10,
